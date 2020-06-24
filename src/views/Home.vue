7e437ddcba930043613a12ff878855c536853cb6
Ankit Singh <ankit@wittypen.com>
2020-06-25 00:09:13 +0530

    setup done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description=""
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.username"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Your Password:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+import axios from 'axios';
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          username: '',
+          password: ''
+          // food: null,
+          //checked: []
+        },
+        // foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault();
+        let userData = {
+          user : {...this.form}
+        }
+        console.log("data sent", userData);
+        axios({
+        url:
+          `https://shipthis.herokuapp.com/api/v1/users`,
+        method: "POST",
+        data: userData
+      }).then(response => {
+          if (response) {
+            console.log("response after update call", response)
+            // this.showContainerToUpdate = false;
+          }
+          // this.getAllAddedSections();
+          // this.$router.go()
+      }).catch(err => {
+        console.log("err no response")
+          throw err;
+        });
+        //alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.username = ''
+        // this.form.food = null
+        // this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
   }
-}
 </script>
+<style scoped>
+.container {
+  border: 1px solid black;
+  width: 500px;
+}
+</style>>
+
+
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // options...
+    devServer: {
+          proxy: 'https://shipthis.herokuapp.com/api/v1/users'
+      }
+  }
